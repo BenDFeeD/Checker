2d8fc54e887f6594eb8c58126e36c0fd1c5a0196
BenD <bendfeed@gmail.com>
2023-11-07 20:23:55 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Checker
+{
+    static void Main()
+    {
+        string Path = @"D:\Загрузки\setup\SDA-1.0.10\Steam Desktop Authenticator.exe"; // Указать путь к программе
+
+        while (true)
+        {
+            if (!IsRunning("Steam Desktop Authenticator")) // Имя программы
+            {
+                Process.Start(Path);
+                Thread.Sleep(30000); // 30 сек
+            }
+            else
+            {
+                Thread.Sleep(300000); // 5 мин
+            }
+        }
+    }
+
+    static bool IsRunning(string Name)
+    {
+        Process[] process = Process.GetProcessesByName(Name);
+        return process.Length > 0;
+    }
+}
